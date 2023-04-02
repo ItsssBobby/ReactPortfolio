179c2bc498f9a18f466aae831a0da34b19201566
ItsssBobby <114122721+ItsssBobby@users.noreply.github.com>
2023-04-01 21:51:22 -0400

    fixed api link in contact for deployment.

--- src/components/contact.js
+++ src/components/contact.js
@@ -25,7 +25,7 @@ function Contact() {
   
     if (formValid) {
       try {
-        const response = await fetch('http://localhost:5000/api/send-email', {
+        const response = await fetch('/api/send-email/', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ name, email, message }),
