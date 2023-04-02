47da685de3784fd8e10335cc58dcfb43e7d5328d
ItsssBobby <114122721+ItsssBobby@users.noreply.github.com>
2023-04-01 20:55:03 -0400

    updates to footer, updated the contact page AND added a working email server so anyone who fill sout the forms and hits submit will send an email directly to me that includes their name, email, and the message.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
 
-// Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [formValid, setFormValid] = useState(false);
+  const [emailValid, setEmailValid] = useState(true);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+      validateEmail(value);
+    } else if (name === "message") {
+      setMessage(value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    if (formValid) {
+      try {
+        const response = await fetch('http://localhost:5000/api/send-email', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name, email, message }),
+        });
+  
+        if (response.ok) {
+          alert('Email sent successfully!');
+        } else {
+          alert('Error sending email. Please try again.');
+        }
+      } catch (error) {
+        console.error('Error sending email:', error);
+        alert('Error sending email. Please try again.');
+      }
+    } else {
+      alert('Please fill out all required fields.');
+    }
+  };
+  
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setEmailValid(emailRegex.test(email));
+  };
+
+  const handleBlur = (e) => {
+    const { name } = e.target;
+
+    if (name === "name") {
+      setFormValid(
+        name.trim() !== "" && email.trim() !== "" && message.trim() !== ""
+      );
+    } else if (name === "email") {
+      setFormValid(
+        name.trim() !== "" &&
+          email.trim() !== "" &&
+          message.trim() !== "" &&
+          emailValid
+      );
+    } else if (name === "message") {
+      setFormValid(
+        name.trim() !== "" && email.trim() !== "" && message.trim() !== ""
+      );
+    }
+  };
+
   return (
-    <div className="mainPage">
-        <h2>
-            Contact Page
-        </h2>
-    </div>
+    <section>
+      <h2>Contact</h2>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+          onBlur={handleBlur}
+          required
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleInputChange}
+          onBlur={handleBlur}
+          required
+        />
+        {!emailValid && (
+          <span className="error-message">
+            Please enter a valid email address.
+          </span>
+        )}
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={message}
+          onChange={handleInputChange}
+          onBlur={handleBlur}
+          required
+        ></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   );
 }
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="footer">
       <ul>
         <li><a className="icon-link" href="https://github.com/ItsssBobby" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
-        <li><a className="icon-link" href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+        <li><a className="icon-link" href="https://www.linkedin.com/in/robert-mcmullen-949a90228/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a></li>
         {/* <li><a className="icon-link" href="https://stackoverflow.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faStackOverflow} /></a></li>
         <li><a className="icon-link" href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a></li> */}
       </ul>
