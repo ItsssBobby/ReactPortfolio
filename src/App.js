01934965fbb258b964890e209417820f57765690
ItsssBobby <114122721+ItsssBobby@users.noreply.github.com>
2023-04-02 20:20:35 -0400

    re arranging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navigation } from "./components/Navigation";
-import AboutMe from "./components/AboutMe";
-import Contact from "./components/Contact";
+import AboutMe from "./components/AboutMe"; 
 import { Resume } from "./components/Resume";
 import Footer from "./components/Footer";
 import "./styles.css";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   const [showViewer, setShowViewer] = useState(false);
--- src/components/contact.js
+++ src/components/contact.js
@@ -119,4 +119,4 @@ const Contact = () => {
   );
 }
 
-export default Contact;
+export default { Contact };
